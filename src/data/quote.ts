1c5f9e4ffd3f32dfe97d9421ac7917c8e531c8ff
Prajwal <prajwalgm256@gmail.com>
2025-12-31 17:30:48 -0500

    upadted quote section data and style

--- src/data/quote.ts
+++ src/data/quote.ts
@@ -1,23 +1,37 @@
 export const quoteData = {
-  quote: {
-    text: "Redefining limits, building intelligent systems, bringing it all together. Defining a legacy in tech.",
-    highlights: ["limits", "intelligent", "legacy"],
-  },
-  
+  quotes: [
+    {
+      text: "I'm not a great programmer, I'm just a good programmer with great habits.",
+      author: "Kent Beck",
+      highlights: ["great", "programmer", "habits", "Kent Beck", "good"],
+    },
+    {
+      text: "The best programs are written so that computing machines can perform them quickly and so that human beings can understand them clearly.",
+      author: "Donald Knuth",
+      highlights: ["best", "programs", "quickly", "clearly"],
+    },
+  ],  
+
   marquee: {
     items: [
-      "FULL STACK",
-      "MACHINE LEARNING", 
-      "DATA ENGINEERING",
-      "REACT",
+      "JAVA",
       "PYTHON",
-      "PYTORCH",
       "TYPESCRIPT",
-      "NORTHEASTERN",
+      "SPRING BOOT",
+      "REACT JS",
+      "NODE.JS",
+      "AWS",
+      "DOCKER",
+      "MICROSERVICES",
+      "REST APIs",
+      "MACHINE LEARNING",
+      "NEXT.JS",
+      "POSTGRESQL",
+      "CI/CD",
     ],
     speed: 25,
   },
-  
+
   timing: {
     quoteDelay: 0.2,
     marqueeDelay: 0.5,
